d1a003b945f4270123edd0e34fd0c81552916b81
Gilles <quodbach.gilles@gmail.com>
2024-01-09 14:00:27 +0100

    partie 1.2 terminée

--- src/City.java
+++ src/City.java
@@ -1,19 +1,55 @@
 
 public class City {
-	//attributs
+	// attributs
 	public String cityName;
 	public String countryName;
-	public int cityResident;
-	
-	//constructeurs
-	public City(String cityName, String countryName, int cityResident ) {
+	private int cityResident;
+
+	// constructeurs
+	public City(String cityName, String countryName, int cityResident) {
 		this.cityName = cityName;
 		this.countryName = countryName;
 		this.cityResident = cityResident;
 	}
-	
-	//méthodes
+
+	// méthodes
 	public void displayCity() {
 		System.out.println(this.cityName + " " + this.countryName + " " + this.cityResident);
 	}
+
+	// accesseurs
+
+	// City name
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	// Country name
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	// City resident
+	public int getCityResident() {
+		return cityResident;
+	}
+
+	public void setCityResident(int cityResident) {
+		if (cityResident < 0) {
+			System.out.println("La population ne peut pas décroître, merci d'entrer un nombre d'habitant valide !");
+			return;
+		} else {
+			this.cityResident += cityResident;
+		}
+	}
+
+
 }
--- src/TestCity.java
+++ src/TestCity.java
@@ -2,17 +2,39 @@
 public class TestCity {
 
 	public static void main(String[] args) {
-		City toulouse = new City("Toulouse", "France", 498003);
+		City toulouse = new City("Toulouse", "France", 500000);
 		City marseille = new City("Marseille", "France", 870321);
 		City lyon = new City("Lyon", "France", 522228);
 
-		toulouse.displayCity();
-		marseille.displayCity();
-		lyon.displayCity();
+		// Sans accesseurs
+//		toulouse.displayCity();
+//		marseille.displayCity();
+//		lyon.displayCity();
+//		
+//		toulouse.cityResident += 20000;
+//		
+//		toulouse.displayCity();
 		
-		toulouse.cityResident += 20000;
+		// Avec accesseurs
+//		toulouse.getCityName();
+//		marseille.getCityName();
+//		lyon.getCityName();
+//		
+//		toulouse.getCountryName();
+//		marseille.getCountryName();
+//		lyon.getCountryName();
+//		
+//		toulouse.getCityResident();
+//		marseille.getCityResident();
+//		lyon.getCityResident();
 		
-		toulouse.displayCity();
+//		toulouse.cityResident = -20000;
+		
+//		System.out.println("La ville de " + toulouse.getCityName() + " a " + toulouse.getCityResident() + " habitants");
+
+		toulouse.setCityResident(-2000);
+//		
+
 	}
 
 }
